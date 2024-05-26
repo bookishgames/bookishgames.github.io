f6b812a950588b38687d0a1e433829ad46b3729b
Vinesh Kannan <vingkan@gmail.com>
2024-05-26 13:28:35 -0700

    feat: Attempt to use device orientation.

--- src/games/jungle/Portal.tsx
+++ src/games/jungle/Portal.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+/* global document */
+import React, { useEffect, useState } from "react";
 
 type PortalProps = {
   destination: string;
 };
 
 export default function Portal({ destination }: PortalProps) {
+  const [data, setData] = useState<string>('');
+  useEffect(() => {
+    const onOrientation = (orientation) => {
+      console.log({ orientation });
+      setData(JSON.stringify(orientation));
+    };
+    document.addEventListener("deviceorientation", onOrientation, true);
+
+    return () => {
+      document.removeEventListener("deviceorientation", onOrientation);
+    };
+  }, [destination]);
+
   return (
     <div className="game-content portal">
       <h2>Portal</h2>
+      <pre>{data || "No Data"}</pre>
     </div>
   );
 }
